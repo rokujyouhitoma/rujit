63125c00c47b6cfaa017833ede0599b9ae7ed302
Masahiro Ide <imasahiro9@gmail.com>
2014-09-08 15:38:41 +0900

    fix indent

--- jit.c
+++ jit.c
@@ -37,7 +37,7 @@ typedef struct jit_event_t {
 static int get_opcode(rb_control_frame_t *cfp, VALUE *reg_pc)
 {
     long pc = (reg_pc - cfp->iseq->iseq_encoded);
-    int op = (int) (cfp->iseq->iseq[pc]);
+    int op = (int)(cfp->iseq->iseq[pc]);
     assert(0 <= op && op < VM_INSTRUCTION_SIZE);
     return op;
 }
@@ -49,15 +49,15 @@ static void dump_inst(jit_event_t *e)
 #if DUMP_INST > 0
     long pc = (e->pc - e->cfp->iseq->iseq_encoded);
     fprintf(stderr, "%04ld pc=%p %02d %s\n",
-	    pc, e->pc, e->opcode, insn_name(e->opcode));
+            pc, e->pc, e->opcode, insn_name(e->opcode));
 #endif
 }
 
 static jit_event_t *jit_init_event(jit_event_t *e, rb_jit_t *jit, rb_thread_t *th, rb_control_frame_t *cfp, VALUE *pc)
 {
-    e->th  = th;
+    e->th = th;
     e->cfp = cfp;
-    e->pc  = pc;
+    e->pc = pc;
     e->opcode = get_opcode(e->cfp, e->pc);
     return e;
 }
@@ -70,11 +70,11 @@ static void jit_global_default_params_setup()
     char *disable_jit_ptr;
     disable_jit_ptr = getenv("RUJIT_DISABLE_JIT");
     if (disable_jit_ptr != NULL) {
-        int disable_jit_i = atoi(disable_jit_ptr);
-        if (RTEST(ruby_verbose)) {
-            fprintf(stderr, "disable_jit=%d\n", disable_jit_i);
-        }
-        disable_jit = disable_jit_i;
+	int disable_jit_i = atoi(disable_jit_ptr);
+	if (RTEST(ruby_verbose)) {
+	    fprintf(stderr, "disable_jit=%d\n", disable_jit_i);
+	}
+	disable_jit = disable_jit_i;
     }
 }
 
@@ -95,17 +95,17 @@ void Init_rawjit()
 {
     jit_global_default_params_setup();
     if (!disable_jit) {
-        current_jit = jit_init();
-        rb_cMath = rb_singleton_class(rb_mMath);
-        Init_jit(); // load jit_prelude
+	current_jit = jit_init();
+	rb_cMath = rb_singleton_class(rb_mMath);
+	Init_jit(); // load jit_prelude
     }
 }
 
 void Destruct_rawjit()
 {
     if (!disable_jit) {
-        jit_delete(current_jit);
-        current_jit = NULL;
+	jit_delete(current_jit);
+	current_jit = NULL;
     }
 }
 
@@ -118,7 +118,7 @@ static VALUE *trace_selection(rb_jit_t *jit, jit_event_t *e)
 VALUE *rb_jit_trace(rb_thread_t *th, rb_control_frame_t *reg_cfp, VALUE *reg_pc)
 {
     if (disable_jit) {
-        return reg_pc;
+	return reg_pc;
     }
     jit_event_t ebuf;
     jit_event_t *e = jit_init_event(&ebuf, current_jit, th, reg_cfp, reg_pc);
