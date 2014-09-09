16736f04054f97d488057c94e65de834073a1359
Masahiro Ide <imasahiro9@gmail.com>
2014-09-09 11:19:27 +0900

    refactor

--- jit.c
+++ jit.c
@@ -118,6 +118,7 @@ typedef enum trace_mode {
 
 struct rb_jit_t {
     VALUE self;
+    trace_t *current_trace;
     trace_recorder_t *recorder;
     trace_mode_t mode;
     hashmap_t traces;
@@ -135,11 +136,11 @@ static int get_opcode(rb_control_frame_t *cfp, VALUE *pc);
 
 static void dump_inst(jit_event_t *e)
 {
-#if DUMP_INST > 0
-    long pc = (e->pc - e->cfp->iseq->iseq_encoded);
-    fprintf(stderr, "%04ld pc=%p %02d %s\n",
-            pc, e->pc, e->opcode, insn_name(e->opcode));
-#endif
+    if (DUMP_INST > 0) {
+	long pc = (e->pc - e->cfp->iseq->iseq_encoded);
+	fprintf(stderr, "%04ld pc=%p %02d %s\n",
+	        pc, e->pc, e->opcode, insn_name(e->opcode));
+    }
 }
 
 static jit_event_t *jit_init_event(jit_event_t *e, rb_jit_t *jit, rb_thread_t *th, rb_control_frame_t *cfp, VALUE *pc)
@@ -147,99 +148,13 @@ static jit_event_t *jit_init_event(jit_event_t *e, rb_jit_t *jit, rb_thread_t *t
     e->th = th;
     e->cfp = cfp;
     e->pc = pc;
+    e->trace = jit->current_trace;
     e->opcode = get_opcode(e->cfp, e->pc);
     return e;
 }
 
 static rb_jit_t *current_jit = NULL;
 static VALUE rb_cMath;
-
-static void jit_global_default_params_setup()
-{
-    char *disable_jit_ptr;
-    disable_jit_ptr = getenv("RUJIT_DISABLE_JIT");
-    if (disable_jit_ptr != NULL) {
-	int disable_jit_i = atoi(disable_jit_ptr);
-	if (RTEST(ruby_verbose)) {
-	    fprintf(stderr, "disable_jit=%d\n", disable_jit_i);
-	}
-	disable_jit = disable_jit_i;
-    }
-}
-
-static void jit_default_params_setup(rb_jit_t *jit)
-{
-}
-
-static void jit_mark(void *ptr)
-{
-    RUBY_MARK_ENTER("jit");
-    if (ptr) {
-    }
-    RUBY_MARK_LEAVE("jit");
-}
-
-static size_t jit_memsize(const void *ptr)
-{
-    size_t size = 0;
-    if (ptr) {
-    }
-    return size;
-}
-
-static const rb_data_type_t jit_data_type = {
-    "JIT",
-    {
-      jit_mark, NULL, jit_memsize,
-    },
-    NULL,
-    NULL,
-    RUBY_TYPED_FREE_IMMEDIATELY
-};
-
-static rb_jit_t *jit_init()
-{
-    rb_jit_t *jit = (rb_jit_t *)malloc(sizeof(*jit));
-    VALUE rb_cJit = rb_define_class("Jit", rb_cObject);
-    rb_undef_alloc_func(rb_cJit);
-    rb_undef_method(CLASS_OF(rb_cJit), "new");
-    jit->self = TypedData_Wrap_Struct(rb_cJit, &jit_data_type, jit);
-    rb_gc_register_mark_object(jit->self);
-
-    hashmap_init(&jit->traces, 1);
-    jit_default_params_setup(jit);
-    return jit;
-}
-
-static void jit_delete(rb_jit_t *jit)
-{
-    if (jit) {
-	// FIXME need to free allocated traces
-	hashmap_dispose(&jit->traces, NULL);
-	free(jit);
-    }
-}
-
-void Init_rawjit()
-{
-    jit_global_default_params_setup();
-    if (!disable_jit) {
-	current_jit = jit_init();
-	rb_cMath = rb_singleton_class(rb_mMath);
-	Init_jit(); // load jit_prelude
-    }
-}
-
-void Destruct_rawjit()
-{
-    if (!disable_jit) {
-	jit_delete(current_jit);
-	current_jit = NULL;
-    }
-    // remove compiler warnings
-    (void)insn_op_types;
-    (void)insn_op_type;
-}
 
 /* memory pool { */
 #define SIZEOF_MEMORY_POOL (4096 - sizeof(void *))
@@ -257,18 +172,22 @@ static void memory_pool_init(struct memory_pool *mp)
 
 static void memory_pool_reset(struct memory_pool *mp, int alloc_memory)
 {
-    struct page_chunk *root = mp->root;
-    struct page_chunk *page = root->next;
-    while (page != NULL) {
-	struct page_chunk *next = page->next;
-	free(page);
-	page = next;
+    struct page_chunk *root;
+    struct page_chunk *page;
+    root = mp->root;
+    if (root) {
+	page = root->next;
+	while (page != NULL) {
+	    struct page_chunk *next = page->next;
+	    free(page);
+	    page = next;
+	}
+	if (!alloc_memory) {
+	    free(root);
+	    root = NULL;
+	}
+	mp->pos = 0;
     }
-    if (!alloc_memory) {
-	free(root);
-	root = NULL;
-    }
-    mp->pos = 0;
     mp->size = SIZEOF_MEMORY_POOL;
     mp->head = mp->root = root;
 }
@@ -343,19 +262,166 @@ CONST_POOL_API void const_pool_delete(const_pool_t *self)
 }
 /* } const pool */
 
+/* trace_recorder { */
+static trace_recorder_t *trace_recorder_new()
+{
+    trace_recorder_t *recorder = (trace_recorder_t *)malloc(sizeof(*recorder));
+    memset(recorder, 0, sizeof(*recorder));
+    memory_pool_init(&recorder->mp);
+    return recorder;
+}
+
+static void trace_recorder_delete(trace_recorder_t *recorder)
+{
+    memory_pool_init(&recorder->mp);
+    free(recorder);
+}
+
+static int trace_recorder_is_full(trace_recorder_t *recorder)
+{
+    /*reserve one instruction for EXIT */
+    return recorder->inst_size - 2 == LIR_MAX_TRACE_LENGTH;
+}
+
+static void compile_trace(rb_jit_t *jit, trace_recorder_t *recorder)
+{
+    fprintf(stderr, "compiled\n");
+    asm volatile("int3");
+}
+
+static VALUE *trace_invoke(rb_jit_t *jit, jit_event_t *e, trace_t *trace)
+{
+    return e->pc;
+}
+
+static void tracebuffer_clear(trace_recorder_t *rec, int alloc_memory)
+{
+    rec->inst_size = 0;
+    memory_pool_reset(&rec->mp, alloc_memory);
+}
+/* } trace_recorder */
+
+/* rb_jit { */
+static void jit_global_default_params_setup()
+{
+    char *disable_jit_ptr;
+    disable_jit_ptr = getenv("RUJIT_DISABLE_JIT");
+    if (disable_jit_ptr != NULL) {
+	int disable_jit_i = atoi(disable_jit_ptr);
+	if (RTEST(ruby_verbose)) {
+	    fprintf(stderr, "disable_jit=%d\n", disable_jit_i);
+	}
+	disable_jit = disable_jit_i;
+    }
+}
+
+static void jit_default_params_setup(rb_jit_t *jit)
+{
+}
+
+static void jit_mark(void *ptr)
+{
+    RUBY_MARK_ENTER("jit");
+    if (ptr) {
+    }
+    RUBY_MARK_LEAVE("jit");
+}
+
+static size_t jit_memsize(const void *ptr)
+{
+    size_t size = 0;
+    if (ptr) {
+    }
+    return size;
+}
+
+static const rb_data_type_t jit_data_type = {
+    "JIT",
+    {
+      jit_mark, NULL, jit_memsize,
+    },
+    NULL,
+    NULL,
+    RUBY_TYPED_FREE_IMMEDIATELY
+};
+
+static rb_jit_t *jit_init()
+{
+    rb_jit_t *jit = (rb_jit_t *)malloc(sizeof(*jit));
+    VALUE rb_cJit = rb_define_class("Jit", rb_cObject);
+    rb_undef_alloc_func(rb_cJit);
+    rb_undef_method(CLASS_OF(rb_cJit), "new");
+    jit->self = TypedData_Wrap_Struct(rb_cJit, &jit_data_type, jit);
+    rb_gc_register_mark_object(jit->self);
+
+    jit->recorder = trace_recorder_new();
+    hashmap_init(&jit->traces, 1);
+    jit_default_params_setup(jit);
+    return jit;
+}
+
+static void jit_delete(rb_jit_t *jit)
+{
+    if (jit) {
+	// FIXME need to free allocated traces
+	hashmap_dispose(&jit->traces, NULL);
+	trace_recorder_delete(jit->recorder);
+	free(jit);
+    }
+}
+
+void Init_rawjit()
+{
+    jit_global_default_params_setup();
+    if (!disable_jit) {
+	current_jit = jit_init();
+	rb_cMath = rb_singleton_class(rb_mMath);
+	Init_jit(); // load jit_prelude
+    }
+}
+
+void Destruct_rawjit()
+{
+    if (!disable_jit) {
+	jit_delete(current_jit);
+	current_jit = NULL;
+    }
+    // remove compiler warnings
+    (void)insn_op_types;
+    (void)insn_op_type;
+}
+/* } rb_jit */
+
 static int is_recording(rb_jit_t *jit)
 {
     return (jit->mode &= TraceModeRecording) == TraceModeRecording;
 }
 
-static void set_recording(rb_jit_t *jit)
+static void set_recording(rb_jit_t *jit, trace_t *trace)
 {
     jit->mode |= TraceModeRecording;
+    jit->current_trace = trace;
 }
 
-static trace_t *create_new_trace(rb_jit_t *jit, jit_event_t *e)
+static void stop_recording(rb_jit_t *jit)
 {
-    return NULL;
+    jit->mode = TraceModeDefault;
+    jit->current_trace = NULL;
+}
+
+static trace_t *create_new_trace(rb_jit_t *jit, jit_event_t *e, trace_side_exit_handler_t *parent)
+{
+    rb_control_frame_t *reg_cfp = e->cfp;
+    trace_t *trace = (trace_t *)malloc(sizeof(*trace));
+    memset(trace, 0, sizeof(*trace));
+    trace->start_pc = e->pc;
+    trace->parent = parent;
+    trace->iseq = GET_ISEQ();
+    const_pool_init(&trace->cpool);
+    if (!parent) {
+	hashmap_set(&jit->traces, (hashmap_data_t)e->pc, (hashmap_data_t)trace);
+    }
+    return trace;
 }
 
 static trace_t *find_trace(rb_jit_t *jit, jit_event_t *e)
@@ -402,11 +468,6 @@ static int already_recorded_on_trace(jit_event_t *e)
 	return 1;
     }
     return 0;
-}
-
-static int trace_recorder_is_full(trace_recorder_t *recorder)
-{
-    return recorder->inst_size - 1 == LIR_MAX_TRACE_LENGTH;
 }
 
 // yarv2lir.c
@@ -478,30 +539,14 @@ static int is_end_of_trace(trace_recorder_t *recorder, jit_event_t *e)
     return 0;
 }
 
-static void append_to_tracebuffer(trace_recorder_t *rec, jit_event_t *e)
-{
-}
-
-static void compile_trace(rb_jit_t *jit, trace_recorder_t *recorder)
-{
-}
-
-static VALUE *trace_invoke(rb_jit_t *jit, jit_event_t *e, trace_t *trace)
-{
-    return e->pc;
-}
-
 static void trace_reset(trace_t *trace, int alloc_memory)
 {
 }
 
-static void tracebuffer_clear(trace_recorder_t *rec, int alloc_memory)
+static void record_inst(trace_recorder_t *ecorder, jit_event_t *e)
 {
 }
-
-static void tracebuffer_set_trace(trace_recorder_t *rec, trace_t *trace)
-{
-}
+//#include "jit_record.c"
 
 static VALUE *trace_selection(rb_jit_t *jit, jit_event_t *e)
 {
@@ -509,10 +554,11 @@ static VALUE *trace_selection(rb_jit_t *jit, jit_event_t *e)
     VALUE *target_pc = NULL;
     if (is_recording(jit)) {
 	if (is_end_of_trace(jit->recorder, e)) {
+	    stop_recording(jit);
 	    compile_trace(jit, jit->recorder);
 	}
 	else {
-	    append_to_tracebuffer(jit->recorder, e);
+	    record_inst(jit->recorder, e);
 	}
 	return e->pc;
     }
@@ -522,18 +568,17 @@ static VALUE *trace_selection(rb_jit_t *jit, jit_event_t *e)
     }
     if (is_backward_branch(e, &target_pc)) {
 	if (!trace) {
-	    trace = create_new_trace(jit, e);
+	    trace = create_new_trace(jit, e, NULL);
 	}
 	trace->last_pc = target_pc;
     }
     if (trace) {
 	trace->counter += 1;
 	if (trace->counter > HOT_TRACE_THRESHOLD) {
-	    set_recording(jit);
-	    tracebuffer_set_trace(jit->recorder, trace);
+	    set_recording(jit, trace);
 	    trace_reset(trace, 1);
 	    tracebuffer_clear(jit->recorder, 1);
-	    append_to_tracebuffer(jit->recorder, e);
+	    record_inst(jit->recorder, e);
 	}
     }
     return e->pc;
